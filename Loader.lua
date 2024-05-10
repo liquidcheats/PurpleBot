7e6dddc617d41eb3c0afcfc88097c257702dc612
liquidcheats <160259130+liquidcheats@users.noreply.github.com>
2024-05-11 02:25:13 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,11 @@
-local repo = loadstring(game:HttpGet("https://raw.githubusercontent.com/liquidcheats/PurpleBot/main/NewMainScript.lua", true))()
+-- 
+
+if game.PlaceId == "6735572261" then
+    local repo = loadstring(game:HttpGet("https://raw.githubusercontent.com/liquidcheats/PurpleBot/main/NewMainScript.lua", true))()
+elseif game.PlaceId == "2788229376" then
+    
+else
+    local repo = loadstring(game:HttpGet("https://raw.githubusercontent.com/liquidcheats/PurpleBot/main/Mobile%20Aimbot.lua", true))()
+end
+
+-- Farwell, Atzlazyblue
