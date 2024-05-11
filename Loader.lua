47b53c5880e1ac8530c6231bd2fe11296fd0d316
liquidcheats <160259130+liquidcheats@users.noreply.github.com>
2024-05-11 13:07:42 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 -- do not discountinue atzlazy blue. 
 
-if game.PlaceId == "6735572261" then
+if game.PlaceId == "6735572261" then -- Pillgrammed
     local repo = loadstring(game:HttpGet("https://raw.githubusercontent.com/liquidcheats/PurpleBot/main/NewMainScript.lua", true))()
-elseif game.PlaceId == "2788229376" then
-    
+elseif game.PlaceId == "2788229376" then -- Da Hood
+    local repo = loadstring(game:HttpGet("", true))()
 else
     local repo = loadstring(game:HttpGet("https://raw.githubusercontent.com/liquidcheats/PurpleBot/main/Mobile%20Aimbot.lua", true))()
 end
